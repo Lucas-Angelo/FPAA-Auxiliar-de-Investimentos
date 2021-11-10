3f01f960a3b8d45cb2b83e00457f090ab5aafc5b
Lucas Ângelo O. M. Rocha <49598959+Lucas-Angelo@users.noreply.github.com>
2021-11-10 15:14:15 -0300

    Leitura de dados e modelo de Ativo

--- src/App.java
+++ src/App.java
@@ -1,8 +1,46 @@
 package src;
-import java.io.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import src.helpers.ArquivoTextoLeitura;
+import src.models.Ativo;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    public static final String arquivo;
+    public static final ArquivoTextoLeitura leitura;
+
+    static {
+        arquivo = "data.csv";
+        leitura = new ArquivoTextoLeitura();
     }
+
+    public static void main(String[] args) {
+        List<Ativo> ativos = preencherVetorAtivo();
+
+        System.out.println(ativos);
+    }
+
+	public static List<Ativo> preencherVetorAtivo() {
+		List<Ativo> ativos = new ArrayList<>();
+
+		leitura.abrirArquivo(arquivo);
+
+		leitura.ler(); // Remove o cabecalho
+        String linha = new String();
+		while (linha != null) {
+            linha = leitura.ler();
+			String[] dadosDaLinha = linha.split(",", 5); // Dividir os dados da linha (5 COLUNAS)
+
+            if(linha!=null)
+                ativos.add(new Ativo(dadosDaLinha[0], dadosDaLinha[1], Double.parseDouble(dadosDaLinha[2]), Double.parseDouble(dadosDaLinha[3]), Double.parseDouble(dadosDaLinha[4])));
+
+            linha = leitura.ler();
+		}
+
+		leitura.fecharArquivo();
+
+		return ativos;
+	}
 }
---
+++ src/models/Ativo.java
@@ -0,0 +1,122 @@
+package src.models;
+
+import java.time.LocalDate;
+
+public class Ativo {
+
+    private static Integer ids;
+    
+    private Integer id;
+    private String nome;
+    private LocalDate data;
+    private double preco;
+    private double valor;
+    private double dividendo; 
+
+    static {
+        ids=0;
+    }
+
+    public void init(String nome, String data, double preco, double valor, double dividendo) {
+        this.id = ids;
+        this.nome = nome;
+        this.data = LocalDate.parse(data);
+        this.preco = preco;
+        this.valor = valor;
+        this.dividendo = dividendo;
+    }
+
+    public Ativo() {
+        ids++;
+    }
+
+    public Ativo(String nome, String data, double preco, double valor, double dividendo) {
+        init(nome, data, preco, valor, dividendo);
+        ids++;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getData() {
+        return this.data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public double getPreco() {
+        return this.preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double getValor() {
+        return this.valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getDividendo() {
+        return this.dividendo;
+    }
+
+    public void setDividendo(double dividendo) {
+        this.dividendo = dividendo;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Ativo))
+            return false;
+        Ativo other = (Ativo) obj;
+        if (this.id == null)
+            if (other.id != null)
+                return false;
+            else if (!this.id.equals(other.id))
+                return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", nome='" + getNome() + "'" +
+            ", data='" + getData() + "'" +
+            ", preco='" + getPreco() + "'" +
+            ", valor='" + getValor() + "'" +
+            ", dividendo='" + getDividendo() + "'" +
+            "}";
+    }
+}
